f128e78c2960551f43f5d2480a647dab19bb0b80
fluflo <96106372+fluflo11@users.noreply.github.com>
2025-11-25 20:46:52 +0000

    Refactor CSV cleaning into a function

--- discard_first.py
+++ discard_first.py
@@ -1,4 +1,9 @@
 import pandas as pd
-df = pd.read_csv('reaction_time_measurements_Florentin.csv') #Just put your name here
-df_clean = df[df['trial'] != 1]
-df_clean.to_csv('fichier_nettoye.csv', index=False)
+
+def clean_csv():
+    df = pd.read_csv('reaction_time_measurements_Florentin.csv') #Just put your name here
+    df_clean = df[df['trial'] != 1]
+    return df_clean
+
+df_clean = clean_csv()
+df_clean.to_csv('clean.csv', index=False)
